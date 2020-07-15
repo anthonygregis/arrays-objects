b2ff35869790d40f1f9b97461580e7428bae75e1
Anthony Gregis <anthony@thedevtron.com>
2020-07-15 16:06:56 -0500

    [JS/HTML] Create variables with console log

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = 'Anthony';
+console.log(name);
+
+var myLocation = 'Cullman';
+console.log(myLocation);
+
+var favoriteNumber = '3';
+console.log(favoriteNumber);
+
+let teams = ['Rockets', 'Lakers', 'Warriors', 'Celtics', 'Blazzers', 'clippers', 'Nets'];
+console.log(teams);
