c55c914707d4e131b1bc436536386bc63ce651ff
Anthony Gregis <anthony@thedevtron.com>
2020-07-15 16:26:42 -0500

    [JS] Add in while loops

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ console.log(favoriteNumber);
 
 let teams = ['Rockets', 'Lakers', 'Warriors', 'Celtics', 'Blazzers', 'clippers', 'Nets'];
 console.log(teams);
+
+// while loops
+var number = 10;
+var otherNumber = 1;
+
+while (otherNumber < number) {
+    console.log(otherNumber)
+    otherNumber++;
+}
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+
+while (myOtherNumber < numberTwo) {
+    console.log(myOtherNumber);
+    myOtherNumber++;
+}
